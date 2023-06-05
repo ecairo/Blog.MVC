e69263bef7da002ae1ba089d2004f5ee4ba36ad9
Cairo <oriac.world2017@gmail.com>
2023-06-05 12:58:40 +0200

    Add Autofac

---
+++ Blog/App_Start/Bootstrapper.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.App_Start
+{
+    public static class Bootstrapper
+    {
+        public static void Run()
+        {
+            SetAutofacContainer();
+        }
+
+        private static void SetAutofacContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Blog.Controllers
 {
     public class HomeController : Controller
     {
+        
+
+        public HomeController()
+        {
+            
+        }
+
         public ActionResult Index()
         {
             return View();
