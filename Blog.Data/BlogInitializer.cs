c30e6a8160ac22ef38528fb5534491f6bb5c9d50
Cairo <oriac.world2017@gmail.com>
2023-06-06 11:47:47 +0200

    Add Db Initizalizer

---
+++ Blog.Data/BlogInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data
+{
+    public class BlogInitializer : DropCreateDatabaseIfModelChanges<BlogEntities>
+    {
+        protected override void Seed(BlogEntities blogEntities)
+        {
+            // ... Inicializar DB con datos de pruebas
+        }
+    }
+}
--- Blog/Global.asax.cs
+++ Blog/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.App_Start;
+using Blog.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,8 @@ namespace Blog
     {
         protected void Application_Start()
         {
+            System.Data.Entity.Database.SetInitializer(new BlogInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
