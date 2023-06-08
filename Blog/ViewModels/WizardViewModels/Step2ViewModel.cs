571433388167967a712cf7d09e8bd44c57bd755c
Cairo <oriac.world2017@gmail.com>
2023-06-08 11:46:55 +0200

    Add validation attribute

--- Blog/ViewModels/WizardViewModels/Step2ViewModel.cs
+++ Blog/ViewModels/WizardViewModels/Step2ViewModel.cs
@@ -9,9 +9,33 @@ namespace Blog.ViewModels.WizardViewModels
     public class Step2ViewModel
     {
         [Required(ErrorMessage = "El DNI es requerido")]
+        [RegularExpression(@"^\d{8}[a-zA-Z]$", ErrorMessage = "El DNI no es válido")]
         public string DNI { get; set; }
 
         [Required(ErrorMessage = "La dirección es requerida")]
         public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "La provincia es requerida")]
+        public string Provincia { get; set; }
+
+        [Required(ErrorMessage = "El municipio es requerido")]
+        [ProvinceValidator] // Aplicando el Validator que hemos hecho
+        public string Municipio { get; set; }
+    }
+
+    public class ProvinceValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var formulario = (Step2ViewModel)validationContext.ObjectInstance;
+
+            // Validar la relación entre municipio y provincia
+            if (formulario.Municipio == "MunicipioEjemplo" && formulario.Provincia != "ProvinciaEjemplo")
+            {
+                return new ValidationResult("El municipio seleccionado no corresponde a la provincia seleccionada.");
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
--- Blog/ViewModels/WizardViewModels/Step3ViewModel.cs
+++ Blog/ViewModels/WizardViewModels/Step3ViewModel.cs
@@ -9,8 +9,10 @@ namespace Blog.ViewModels.WizardViewModels
     public class Step3ViewModel
     {
         [Required(ErrorMessage = "El número de cuenta es requerido")]
-        public string NumeroCuenta { get; set; }        
+        public string NumeroCuenta { get; set; }
 
+        [Required(ErrorMessage = "Debes aceptar los términos y condiciones")]
+        [Display(Name = "Acepto los términos y condiciones")]
         public bool AceptarTerminos { get; set; }
     }
 }
